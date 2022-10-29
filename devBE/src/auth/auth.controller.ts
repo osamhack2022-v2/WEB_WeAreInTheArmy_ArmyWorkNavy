1cfe89682c310efcf345144a0a051feee4f026b6
mireflare <mireflare@gmail.com>
2022-10-29 14:30:40 +0900

    repository moved

--- devBE/src/auth/auth.controller.ts
+++ devBE/src/auth/auth.controller.ts
@@ -1,26 +1,31 @@
 import {
-	Body,
 	Controller,
 	Get,
 	Post,
+	Body,
+	Patch,
+	Param,
+	Delete,
+	ValidationPipe,
 	Req,
 	UseGuards,
-	ValidationPipe,
 } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateAuthDto } from './dto/update-user.dto';
+import { AuthCredentialsDto } from './dto/auth-credentials.dto';
 import { AuthGuard } from '@nestjs/passport';
 import { ApiOperation, ApiTags } from '@nestjs/swagger';
-import { AuthService } from './auth.service';
-import { AuthCredentialsDto } from './dto/auth-credentials.dto';
-import { CreateUserDto } from './dto/create-user.dto';
+import { User } from './entities/users.entity';
 
-@Controller('absproxy/3000/api/auth/')
+@Controller('api/auth/')
 @ApiTags('Users and Authentication API')
 export class AuthController {
-	constructor(private readonly authService: AuthService) {}
+	constructor(private readonly authService: AuthService) { }
 
 	@Post('/signup')
 	@ApiOperation({ summary: 'createUser API' })
-	signUp(@Body(ValidationPipe) createUserDto: CreateUserDto): Promise<void> {
+	signUp(@Body(ValidationPipe) createUserDto: CreateUserDto): Promise<CreateUserDto> {
 		return this.authService.signUp(createUserDto);
 	}
 
@@ -28,41 +33,42 @@ export class AuthController {
 	@ApiOperation({ summary: 'authenticateCredentials API' })
 	signIn(
 		@Body(ValidationPipe) authCredentialsDto: AuthCredentialsDto,
-	): Promise<{ accessToken: string }> {
+	): Promise<{ user: User, accessToken: string }> {
 		return this.authService.signIn(authCredentialsDto);
 	}
 
-	@Post('/test')
-	@UseGuards(AuthGuard())
-	@ApiOperation({ summary: 'get user object from authenticated client API' })
-	test(@Req() req) {
-		console.log('req', req.user);
+	@Get('/getUserInfo/:identifier')
+	@ApiOperation({ summary: 'get user object from identifier' })
+	getUserInfo(@Param("identifier") identifier: string): Promise<User> {
+		return this.authService.getUserInfo(identifier);
 	}
 
-	@Get('/hello')
-	hello() {
-		return 'hello';
+	@Get('/getUserInfoByAuth')
+	@UseGuards(AuthGuard())
+	@ApiOperation({ summary: 'get user object from authenticated client API' })
+	getUserInfoByAuth(@Req() req): Promise<User> {
+		return req.user;
 	}
 
 	/*
   @Get()
   findAll() {
-    return this.authService.findAll();
+	return this.authService.findAll();
   }
 
   @Get(':id')
   findOne(@Param('id') id: string) {
-    return this.authService.findOne(+id);
+	return this.authService.findOne(+id);
   }
 
   @Patch(':id')
   update(@Param('id') id: string, @Body() updateAuthDto: UpdateAuthDto) {
-    return this.authService.update(+id, updateAuthDto);
+	return this.authService.update(+id, updateAuthDto);
   }
 
   @Delete(':id')
   remove(@Param('id') id: string) {
-    return this.authService.remove(+id);
+	return this.authService.remove(+id);
   }
   */
 }
--- devBE/src/board/board.controller.ts
+++ devBE/src/board/board.controller.ts
@@ -1,19 +1,4 @@
-import {
-	Body,
-	Controller,
-	Delete,
-	Get,
-	Param,
-	ParseIntPipe,
-	Patch,
-	Post,
-	Req,
-	UploadedFiles,
-	UseGuards,
-	UseInterceptors,
-	UsePipes,
-	ValidationPipe,
-} from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, ValidationPipe, UsePipes, ParseIntPipe, UseGuards, Req, UseInterceptors, UploadedFiles } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 import { FilesInterceptor } from '@nestjs/platform-express';
 import { ApiTags } from '@nestjs/swagger';
@@ -22,114 +7,132 @@ import { BoardService } from './board.service';
 import { CreateBoardDto } from './dto/create-board.dto';
 import { UpdateBoardDto } from './dto/update-board.dto';
 import { Board } from './entities/board.entity';
+import { UnitJoinDto } from './dto/unit-join.dto';
+import { SoldierJoinDto } from './dto/soldier-join.dto';
+import { SetStatusDto } from './dto/set-status.dto';
 
-@Controller('/absproxy/3000/api/board')
-@ApiTags('Boards API')
+@Controller('/api/board')
+@ApiTags("Boards API")
 @UseGuards(AuthGuard())
 export class BoardController {
-	constructor(private readonly boardService: BoardService) { }
+  constructor(private readonly boardService: BoardService) { }
 
-	@Post('/createBoard')
-	@UsePipes(ValidationPipe)
-	createBoard(
-		@Body() createBoardDto: CreateBoardDto,
-		@Req() req,
-	): Promise<Board> {
-		return this.boardService.createBoard(createBoardDto, req.user);
-	}
+  @Post("/createBoard")
+  @UsePipes(ValidationPipe)
+  createBoard(@Body() createBoardDto: CreateBoardDto,
+    @Req() req): Promise<Board> {
+    return this.boardService.createBoard(createBoardDto, req.user);
+  }
 
-	@Delete('/deleteBoard/:idx')
-	deleteBoard(
-		@Param('idx', ParseIntPipe) idx: number,
-		@Req() req,
-	): Promise<void> {
-		return this.boardService.deleteBoard(idx, req.user);
-	}
+  @Delete("/deleteBoard/:idx")
+  deleteBoard(@Param("idx", ParseIntPipe) idx: number, @Req() req): Promise<void> {
+    return this.boardService.deleteBoard(idx, req.user);
+  }
 
-	@Patch('/updateBoard/:idx')
-	@UsePipes(ValidationPipe)
-	updateBoard(
-		@Param('idx', ParseIntPipe) idx: number,
-		@Body() updateBoardDto: UpdateBoardDto,
-		@Req() req,
-	): Promise<Board> {
-		return this.boardService.updateBoard(idx, updateBoardDto, req.user);
-	}
+  @Patch("/updateBoard/:idx")
+  @UsePipes(ValidationPipe)
+  updateBoard(
+    @Param('idx', ParseIntPipe) idx: number,
+    @Body() updateBoardDto: UpdateBoardDto,
+    @Req() req
+  ): Promise<Board> {
+    return this.boardService.updateBoard(idx, updateBoardDto, req.user);
+  }
 
-	@Get('/getAllBoards')
-	getAllBoards(): Promise<Board[]> {
-		return this.boardService.getAllBoards();
-	}
+  @Get("/getAllBoards")
+  getAllBoards(): Promise<Board[]> {
+    return this.boardService.getAllBoards();
+  }
 
-	@Get('/getBoardsByAuth')
-	getBoardsById(@Req() req): Promise<Board[]> {
-		return this.boardService.getBoardsbyId(req.user);
-	}
+  @Get("/getAllUndoneBoards")
+  getAllUndoneBoards(): Promise<Board[]> {
+    return this.boardService.getAllUndoneBoards();
+  }
 
-	@Get('/getBoardByIndex/:idx')
-	getBoardByIdx(@Param('idx') idx: number): Promise<Board> {
-		return this.boardService.getBoardByIdx(idx);
-	}
+  @Get("/getBoardsByIdentifier/:identifier")
+  getBoardsByIdentifier(@Param("identifier") identifier: string, @Req() req): Promise<Board[]> {
+    return this.boardService.getBoardsbyIdentifier(identifier);
+  }
 
-	@Get('/getAcceptedBoards')
-	getAcceptedBoard(@Req() req): Promise<Board[]> {
-		return this.boardService.getAcceptedBoards(req.user);
-	}
+  @Get("/getBoardsByAuth")
+  getBoardsById(@Req() req): Promise<Board[]> {
+    return this.boardService.getBoardsbyIdentifier(req.user.identifier);
+  }
 
-	@Get('/getBoardsByAcceptor/:identifier')
-	getBoardBy(@Param('identifier') identifier: string): Promise<Board[]> {
-		return this.boardService.getBoardsByAcceptor(identifier);
-	}
+  @Get("/getBoardByIndex/:idx")
+  getBoardByIdx(@Param("idx") idx: number): Promise<Board> {
+    return this.boardService.getBoardByIdx(idx);
+  }
 
-	@Post('/uploadImages')
-	@UseInterceptors(FilesInterceptor('images', 10, multerConfigs))
-	// FileInterceptors("Key value of the from", "limit for the number of files", "file upload configure")
-	uploadImages(@UploadedFiles() files: File[]): object {
-		return this.boardService.uploadImages(files);
-	}
+  @Post("/uploadImages")
+  @UseInterceptors(FilesInterceptor("images", 10, multerConfigs))
+  // FileInterceptors("Key value of the from", "limit for the number of files", "file upload configure")
+  uploadImages(
+    @UploadedFiles() files: File[]
+  ): object {
+    return this.boardService.uploadImages(files);
+  }
 
-	@Patch('/acceptRequest/:idx')
-	@UsePipes(ValidationPipe)
-	acceptRequest(
-		@Param('idx', ParseIntPipe) idx: number,
-		@Req() req,
-	): Promise<Board> {
-		return this.boardService.acceptRequest(idx, req.user);
-	}
+  @Patch("/unitParticipate")
+  @UsePipes(ValidationPipe)
+  unitParticipate(@Body() unitJoinDto: UnitJoinDto, @Req() req): Promise<Board> {
+    return this.boardService.unitParticipate(unitJoinDto, req.user);
+  }
 
-	@Patch('/cancelRequest/:idx')
-	@UsePipes(ValidationPipe)
-	cancelRequest(
-		@Param('idx', ParseIntPipe) idx: number,
-		@Req() req,
-	): Promise<Board> {
-		return this.boardService.cancelRequest(idx, req.user);
-	}
+  @Patch("/unitCancelParticipation/:idx")
+  @UsePipes(ValidationPipe)
+  unitCancelParticipation(@Param("idx") idx:number, @Req() req): Promise<Board> {
+    return this.boardService.unitCancelParticipation(idx, req.user);
+  }
 
-	/*
+  @Patch("/soldierParticipate")
+  @UsePipes(ValidationPipe)
+  soldierParticipate(@Body() soldierJoinDto: SoldierJoinDto, @Req() req): Promise<Board> {
+    return this.boardService.soldierParticipate(soldierJoinDto, req.user);
+  }
+
+  @Patch("/soldierCancelParticipation/:idx")
+  @UsePipes(ValidationPipe)
+  soldierCancelParticipation(@Param("idx") idx:number, @Req() req): Promise<Board> {
+    return this.boardService.soldierCancelParticipation(idx, req.user);
+  }
+
+  @Patch("/setStatus")
+  @UsePipes(ValidationPipe)
+  setStatus(@Body() setStatusDto: SetStatusDto, @Req() req): Promise<Board> {
+    return this.boardService.setStatus(setStatusDto, req.user);
+  }
+
+  @Patch("/setDone/:idx")
+  @UsePipes(ValidationPipe)
+  setDone(@Param("idx") idx: number, @Req() req): Promise<Board> {
+    return this.boardService.setDone(idx, req.user);
+  }
+
+  /*
   @Post()
   create(@Body() createBoardDto: CreateBoardDto) {
-	return this.boardService.create(createBoardDto);
+    return this.boardService.create(createBoardDto);
   }
 
   @Get()
   findAll() {
-	return this.boardService.findAll();
+    return this.boardService.findAll();
   }
 
   @Get(':id')
   findOne(@Param('id') id: string) {
-	return this.boardService.findOne(+id);
+    return this.boardService.findOne(+id);
   }
 
   @Patch(':id')
   update(@Param('id') id: string, @Body() updateBoardDto: UpdateBoardDto) {
-	return this.boardService.update(+id, updateBoardDto);
+    return this.boardService.update(+id, updateBoardDto);
   }
 
   @Delete(':id')
   remove(@Param('id') id: string) {
-	return this.boardService.remove(+id);
+    return this.boardService.remove(+id);
   }
   */
 }
